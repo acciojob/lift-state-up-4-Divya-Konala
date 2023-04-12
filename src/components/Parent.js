eb954b22d220d3cabab72d871e9e4dc0d6647003
Divya-Konala <87460968+Divya-Konala@users.noreply.github.com>
2023-04-12 16:36:49 +0530

    use state 4

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import Child from './Child';
 
 const Parent=()=>{
-    const [cartItems,setCartItems]=useState([])
+    const [cartItems,setCartItems]=useState([{ id: 1, name: "Item 1", price: 10 },
+    { id: 2, name: "Item 2", price: 20 },
+    { id: 3, name: "Item 3", price: 30 }])
     const [itemName,setItemName]=useState("");
     const [itemPrice,setItemPrice]=useState("");
 
